9e38557525a2571226773f7350c5cbb06f8a1cec
tv-rom <afuttersak@gmail.,com>
2020-08-25 15:55:42 -0400

    dd

--- sketch.js
+++ sketch.js
@@ -12,13 +12,20 @@ function setup() {
 
 function draw() {
 
-  orbitControl();
+  //orbitControl();
+background('rgba(0,255,0, 0.25)');
+    let locX = mouseX - height / 2;
+  let locY = mouseY - width / 2;
+
+ // ambientLight(60, 60, 60);
+  pointLight(255, 255, 255, locX, locY, 100);
 
 
-  rotateX(frameCount * 0.01);
-   rotateY(frameCount * 0.01);
-  rotateY(mouseX/1000);
-  scale(displayWidth/6);
-   normalMaterial();
+  rotateX(180);
+   rotateY(frameCount * -0.01);
+  //rotateY(mouseX/1000);
+  scale(displayWidth/16);
+  // normalMaterial();
+  specularMaterial(0);
   model(line);
 }
