2fb7416f678acaa53a77e0cdf798a098c532f2a6
tv-rom <afuttersak@gmail.,com>
2020-08-25 15:13:50 -0400

    s

--- sketch.js
+++ sketch.js
@@ -10,8 +10,8 @@ function setup() {
 
 function draw() {
    background(200);
-  rotateX(frameCount * mouseX/10);
-  rotateY(frameCount * mouseY/10);
+  rotateX(mouseX/10);
+  rotateY(mouseY/10);
   scale(24);
    normalMaterial();
   model(line);
